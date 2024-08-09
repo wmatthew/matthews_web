e4daff304642b03c87d827faf0fc30785eda941c
Matthew Webber <283947+wmatthew@users.noreply.github.com>
2024-08-09 11:13:30 -0700

    fix x-linking issues

--- eleventy/backendJS/Connections.js
+++ eleventy/backendJS/Connections.js
@@ -5,46 +5,48 @@ var constraintMetadata = JSON.parse(fs.readFileSync("_data/constraints-metadata.
 var boardLib = JSON.parse(fs.readFileSync("_data/board-library.json", 'utf8'));
 var pieceLib = JSON.parse(fs.readFileSync("_data/piece-library.json", 'utf8'));
 
-var justTheTemplates = {}
-Object.values(constraintTemplates).filter(obj => "type" in obj && obj.type == "Template").forEach(obj => { justTheTemplates[obj.key] = obj; });
-
 module.exports = class Connections {
 
     static getUrl(key) {
         // Figure out if this is a board, constraint, constraintKey, or piece and return the right URL for it.
+        // TODO: unit test: make sure no two entities share the same key.
         if (key in boardLib) {
             return "/board/gallery/#:~:text=" + key;
         }
 
-        // TODO: unit test: make sure no two entities share the same key.
-        if (key in justTheTemplates) {
-            var name = justTheTemplates[key].name;
-            name = name.replace("-", "%2D"); // https://web.dev/articles/text-fragments#:~:text=This%20is%20especially%20important
-            return "/investigations/#:~:text=" + name;
+        if (key in constraintTemplates) {
+            return "/investigations/#:~:text=Key: " + escape(key);
         }
 
         if (key in constraintLibrary) {
-            // TODO: jump to the right item
-            return "/investigations/#" + key;
+            return "/investigations/#:~:text=Key: " + escape(key);
         }
 
         if (key in pieceLib) {
-            return "/polycube/gallery/#:~:text=Key: " + key;
+            return "/polycube/gallery/#:~:text=Key: " + escape(key);
         }
 
         if (key in constraintMetadata.Tiling_Default.constraint_flags) {
-            return "/tiling/constraints/#:~:text=" + key;
+            return "/tiling/constraints/#:~:text=" + escape(key);
         }
 
         throw new Error("Could not determine URL for key: " + key);
+
+        function escape(rawString) {
+            if (rawString.includes("-")) {
+                console.log("escaping dashes: before: " + rawString);
+                console.log("escaping dashes: after: " + rawString.replace("-", "%2D"));
+            }
+            return rawString.replace("-", "%2D"); // https://web.dev/articles/text-fragments#:~:text=This%20is%20especially%20
+        }
     }
 
     static getThingsThatUsePiece(pieceKey) {
-        var result = Object.values(justTheTemplates).concat(Object.values(constraintLibrary)).filter(obj => {
+        var result = Object.values(constraintTemplates).concat(Object.values(constraintLibrary)).filter(obj => {
             return ("key" in obj) &&
             ("type" in obj) &&
             ("pieceSupply" in obj) &&
-            ["Template", "Custom", "Generated"].includes(obj.type) &&
+            ["Template", "Generated"].includes(obj.type) &&
             Array.isArray(obj.pieceSupply) &&
             (obj.pieceSupply.map(p => p[0]).includes(pieceKey));
         }
@@ -53,24 +55,14 @@ module.exports = class Connections {
     }
 
     static getThingsThatUseBoard(boardKey) {
-        // TODO: inconsistent. In templates, board is a string. In library, board is an object.
-        // We should have two fields: boardKey and boardObject.
 
-        var result1 = Object.values(justTheTemplates).filter(obj => {
-            return ("key" in obj) &&
-            ("type" in obj) &&
-            ["Template", "Custom"].includes(obj.type) &&
-            obj.boardKey == boardKey;
-        }
-        ).map(obj => {return {url:Connections.getUrl(obj.key), displayName:obj.name};});
-
-        var result2 = Object.values(constraintLibrary).filter(obj => {
+        var result = Object.values(constraintLibrary).filter(obj => {
             return ("key" in obj) &&
             obj.boardKey == boardKey;
         }
         ).map(obj => {return {url:Connections.getUrl(obj.key), displayName:obj.name};});
 
-        return result1.concat(result2);
+        return result;
     }
 
     static getConstraintTemplatesThatUseKey(key) {
